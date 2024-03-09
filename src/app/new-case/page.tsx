384da826fd9a9664dfc7154f6f3a8a4ef6a4c131
Navneet <amitkawasthi1@gmail.com>
2024-03-09 12:59:58 +0530

    new chnages

--- src/app/new-case/page.tsx
+++ src/app/new-case/page.tsx
@@ -1,4 +1,4 @@
-// // Import necessary dependencies
+// Import necessary dependencies
 // import pageTitle from "@/common/utils/page-title";
 // import { ContactCard, NewCaseForm } from "@/components/new-case";
 // import { Metadata } from "next";
@@ -8,50 +8,51 @@
 // // Define metadata
 // export const metadata: Metadata = pageTitle.newCase;
 
-// // Define NewCasePage component
-// const NewCasePage = () => {
-//     return (
-//         <section>
-//             {/* Page Heading */}
-//             <PageHeading className="text-start">New Case</PageHeading>
+// Define NewCasePage component
+const NewCasePage = () => {
+    return (
+      <></>
+      //   <section>
+      //       {/* Page Heading */}
+      //       <PageHeading className="text-start">New Case</PageHeading>
 
-//             {/* Page Content */}
-//             <div className="max-w-[1440px] mx-auto">
-//                 <div className="px-4 py-6 md:px-[50px] lg:px-[100px] md:pt-[60px] md:pb-[80px] ">
-//                     <p className="animate__fadeInUp animate__animated animate__delay-500ms">
-//                         Fill out some info and we will be in touch shortly! We can't wait to hear from you!
-//                     </p>
-//                 </div>
+      //       {/* Page Content */}
+      //       <div className="max-w-[1440px] mx-auto">
+      //           <div className="px-4 py-6 md:px-[50px] lg:px-[100px] md:pt-[60px] md:pb-[80px] ">
+      //               <p className="animate__fadeInUp animate__animated animate__delay-500ms">
+      //                   Fill out some info and we will be in touch shortly! We can't wait to hear from you!
+      //               </p>
+      //           </div>
 
-//                 {/* New Case Form */}
-//                 <NewCaseForm />
+      //           {/* New Case Form */}
+      //           <NewCaseForm />
 
-//                 {/* Contact Details */}
-//                 <div className="px-4 md:px-[50px] lg:px-[100px] my-[40px] md:my-[80px]">
-//                     <h2 className="text-[32px] md:text-[46px] font-bold mb-[30px] md:mb-[60px]" data-aos="fade-up">
-//                         Contact Details
-//                     </h2>
-//                     <div className="grid xl:grid-cols-3 md:grid-cols-2 gap-[10px] md:gap-[20px] md:justify-between">
-//                         {contactDetails.map((contact, index) => (
-//                             <ContactCard
-//                                 key={contact.title}
-//                                 card={contact}
-//                                 className="max-w-full"
-//                                 dataAos="zoom-in"
-//                                 dataAosDelay={150 * index}
-//                                 href={contact.href}
-//                             />
-//                         ))}
-//                     </div>
-//                 </div>
-//             </div>
-//         </section>
-//     );
-// };
+      //           {/* Contact Details */}
+      //           <div className="px-4 md:px-[50px] lg:px-[100px] my-[40px] md:my-[80px]">
+      //               <h2 className="text-[32px] md:text-[46px] font-bold mb-[30px] md:mb-[60px]" data-aos="fade-up">
+      //                   Contact Details
+      //               </h2>
+      //               <div className="grid xl:grid-cols-3 md:grid-cols-2 gap-[10px] md:gap-[20px] md:justify-between">
+      //                   {contactDetails.map((contact, index) => (
+      //                       <ContactCard
+      //                           key={contact.title}
+      //                           card={contact}
+      //                           className="max-w-full"
+      //                           dataAos="zoom-in"
+      //                           dataAosDelay={150 * index}
+      //                           href={contact.href}
+      //                       />
+      //                   ))}
+      //               </div>
+      //           </div>
+      //       </div>
+      //   </section>
+    );
+};
 
-// export default NewCasePage;
+export default NewCasePage;
 
-// // Contact Details data
+// Contact Details data
 // const contactDetails = [
 //     {
 //         icon: <LocationIconNewCase />,
