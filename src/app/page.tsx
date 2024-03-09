7df698720d24b7abcc30cb80f12cd49ba3230d45
Navneet <amitkawasthi1@gmail.com>
2024-03-09 13:06:21 +0530

    new change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,31 +11,32 @@ import {
 } from "@/components/home";
 import { Metadata } from "next";
 import pageTitle from "@/common/utils/page-title";
-import { getAllPosts } from "@/common/apis/post.api";
+// import { getAllPosts } from "@/common/apis/post.api";
 import NewTimeLine from "@/components/home/NewTimeline";
 
 // export const metadata: Metadata = pageTitle.home;
 
 const HomePage = async () => {
-   const { posts } = await getAllPosts(1, 10);
+   // const { posts } = await getAllPosts(1, 10);
 
    return (
       <div className="min-h-[90vh]  bg-[#F3F3F3]">
+         <h1>hello</h1>
          {/* <HomeBanner /> */}
-         <NewHomeBanner />
+         {/* <NewHomeBanner />
          <Container>
             <WhoWeAre />
-            <OurServices />
+            <OurServices /> */}
             {/* <HowWeWorkTimeline /> */}
-            <NewTimeLine />
-         </Container>
+            {/* <NewTimeLine />
+         </Container> */}
 
          <div className="bg-[#F6F6F6]">
-            <Container>
+            {/* <Container>
                <ContactUs />
                <FromOurBlog posts={posts} />
             </Container>
-            <Sponsors />
+            <Sponsors /> */}
          </div>
       </div>
    );
