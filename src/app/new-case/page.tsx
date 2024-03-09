5b9538c380c6886d360938fab6e31a94f5bb8f1f
Navneet <amitkawasthi1@gmail.com>
2024-03-09 12:52:51 +0530

    changes

--- src/app/new-case/page.tsx
+++ src/app/new-case/page.tsx
@@ -1,74 +1,74 @@
-// Import necessary dependencies
-import pageTitle from "@/common/utils/page-title";
-import { ContactCard, NewCaseForm } from "@/components/new-case";
-import { Metadata } from "next";
-import PageHeading from "@/components/pageHeading";
-import { CallIconNewCase, LocationIconNewCase, MailIconNewCase } from "@/components/icons";
+// // Import necessary dependencies
+// import pageTitle from "@/common/utils/page-title";
+// import { ContactCard, NewCaseForm } from "@/components/new-case";
+// import { Metadata } from "next";
+// import PageHeading from "@/components/pageHeading";
+// import { CallIconNewCase, LocationIconNewCase, MailIconNewCase } from "@/components/icons";
 
-// Define metadata
-export const metadata: Metadata = pageTitle.newCase;
+// // Define metadata
+// export const metadata: Metadata = pageTitle.newCase;
 
-// Define NewCasePage component
-const NewCasePage = () => {
-    return (
-        <section>
-            {/* Page Heading */}
-            <PageHeading className="text-start">New Case</PageHeading>
+// // Define NewCasePage component
+// const NewCasePage = () => {
+//     return (
+//         <section>
+//             {/* Page Heading */}
+//             <PageHeading className="text-start">New Case</PageHeading>
 
-            {/* Page Content */}
-            <div className="max-w-[1440px] mx-auto">
-                <div className="px-4 py-6 md:px-[50px] lg:px-[100px] md:pt-[60px] md:pb-[80px] ">
-                    <p className="animate__fadeInUp animate__animated animate__delay-500ms">
-                        Fill out some info and we will be in touch shortly! We can't wait to hear from you!
-                    </p>
-                </div>
+//             {/* Page Content */}
+//             <div className="max-w-[1440px] mx-auto">
+//                 <div className="px-4 py-6 md:px-[50px] lg:px-[100px] md:pt-[60px] md:pb-[80px] ">
+//                     <p className="animate__fadeInUp animate__animated animate__delay-500ms">
+//                         Fill out some info and we will be in touch shortly! We can't wait to hear from you!
+//                     </p>
+//                 </div>
 
-                {/* New Case Form */}
-                <NewCaseForm />
+//                 {/* New Case Form */}
+//                 <NewCaseForm />
 
-                {/* Contact Details */}
-                <div className="px-4 md:px-[50px] lg:px-[100px] my-[40px] md:my-[80px]">
-                    <h2 className="text-[32px] md:text-[46px] font-bold mb-[30px] md:mb-[60px]" data-aos="fade-up">
-                        Contact Details
-                    </h2>
-                    <div className="grid xl:grid-cols-3 md:grid-cols-2 gap-[10px] md:gap-[20px] md:justify-between">
-                        {contactDetails.map((contact, index) => (
-                            <ContactCard
-                                key={contact.title}
-                                card={contact}
-                                className="max-w-full"
-                                dataAos="zoom-in"
-                                dataAosDelay={150 * index}
-                                href={contact.href}
-                            />
-                        ))}
-                    </div>
-                </div>
-            </div>
-        </section>
-    );
-};
+//                 {/* Contact Details */}
+//                 <div className="px-4 md:px-[50px] lg:px-[100px] my-[40px] md:my-[80px]">
+//                     <h2 className="text-[32px] md:text-[46px] font-bold mb-[30px] md:mb-[60px]" data-aos="fade-up">
+//                         Contact Details
+//                     </h2>
+//                     <div className="grid xl:grid-cols-3 md:grid-cols-2 gap-[10px] md:gap-[20px] md:justify-between">
+//                         {contactDetails.map((contact, index) => (
+//                             <ContactCard
+//                                 key={contact.title}
+//                                 card={contact}
+//                                 className="max-w-full"
+//                                 dataAos="zoom-in"
+//                                 dataAosDelay={150 * index}
+//                                 href={contact.href}
+//                             />
+//                         ))}
+//                     </div>
+//                 </div>
+//             </div>
+//         </section>
+//     );
+// };
 
-export default NewCasePage;
+// export default NewCasePage;
 
-// Contact Details data
-const contactDetails = [
-    {
-        icon: <LocationIconNewCase />,
-        title: "Location",
-        textOne: "Merlin, Hunter & Associates Limited",
-        href: "https://www.google.com/maps/place/St+Katharine's+Way,+London,+UK/@51.5064904,-0.0729412,19z/data=!3m1!4b1!4m10!1m2!2m1!1sSt.+Katherine's+Way+International+House,+LDN+EIW+1+YL!3m6!1s0x487603483289ff35:0x6bdada3fe3f5b8ed!8m2!3d51.5064904!4d-0.0717235!15sCjlTdC4gS2F0aGFyaW5lJ3MgV2F5IEludGVybmF0aW9uYWwgSG91c2UsIExPTkRPTiBWSUVXIDEgWUySAQVyb3V0ZeABAA!16s%2Fg%2F1tdwddn_?entry=ttu",
-    },
-    {
-        icon: <MailIconNewCase />,
-        title: "E-mail Address",
-        textOne: " charles@merlinhunter.com",
-        href: "mailto:charles@merlinhunter.com",
-    },
-    {
-        icon: <CallIconNewCase />,
-        title: "Phone Number",
-        textOne: "+44 020 3340 6070",
-        href: "tel:+44 020 3340 6070",
-    },
-];
+// // Contact Details data
+// const contactDetails = [
+//     {
+//         icon: <LocationIconNewCase />,
+//         title: "Location",
+//         textOne: "Merlin, Hunter & Associates Limited",
+//         href: "https://www.google.com/maps/place/St+Katharine's+Way,+London,+UK/@51.5064904,-0.0729412,19z/data=!3m1!4b1!4m10!1m2!2m1!1sSt.+Katherine's+Way+International+House,+LDN+EIW+1+YL!3m6!1s0x487603483289ff35:0x6bdada3fe3f5b8ed!8m2!3d51.5064904!4d-0.0717235!15sCjlTdC4gS2F0aGFyaW5lJ3MgV2F5IEludGVybmF0aW9uYWwgSG91c2UsIExPTkRPTiBWSUVXIDEgWUySAQVyb3V0ZeABAA!16s%2Fg%2F1tdwddn_?entry=ttu",
+//     },
+//     {
+//         icon: <MailIconNewCase />,
+//         title: "E-mail Address",
+//         textOne: " charles@merlinhunter.com",
+//         href: "mailto:charles@merlinhunter.com",
+//     },
+//     {
+//         icon: <CallIconNewCase />,
+//         title: "Phone Number",
+//         textOne: "+44 020 3340 6070",
+//         href: "tel:+44 020 3340 6070",
+//     },
+// ];
--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -1,26 +1,26 @@
-"use client";
-import pageTitle from "@/common/utils/page-title";
-import PageHeading from "@/components/pageHeading";
-import Work from "@/components/work";
-import Notice from "@/components/work/Notice";
-import { Metadata } from "next";
-import { useEffect } from "react";
+// "use client";
+// import pageTitle from "@/common/utils/page-title";
+// import PageHeading from "@/components/pageHeading";
+// import Work from "@/components/work";
+// import Notice from "@/components/work/Notice";
+// import { Metadata } from "next";
+// import { useEffect } from "react";
 
-// export const metadata: Metadata = pageTitle.work;
+// // export const metadata: Metadata = pageTitle.work;
 
-const WorkPage = () => {
-   // useEffect(() => {
-   //    if (typeof window !== 'undefined') {
-   //      // Code that relies on document object (only runs in the browser)
-   //      document.title = "My Dynamic Title";
-   //    }
-   //  }, []);
-   return (
-      <div>
-         <PageHeading>Process</PageHeading>
-         <Work />
-      </div>
-   );
-};
+// const WorkPage = () => {
+//    useEffect(() => {
+//       if (typeof window !== 'undefined') {
+//         // Code that relies on document object (only runs in the browser)
+//         document.title = "My Dynamic Title";
+//       }
+//     }, []);
+//    return (
+//       <div>
+//          <PageHeading>Process</PageHeading>
+//          <Work />
+//       </div>
+//    );
+// };
 
-export default WorkPage;
+// export default WorkPage;
