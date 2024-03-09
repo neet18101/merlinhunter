0e11b7953fde570e8672161fd2c90e2753ef680f
Navneet <amitkawasthi1@gmail.com>
2024-03-09 12:43:37 +0530

    new changes

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -1,12 +1,20 @@
+"use client";
 import pageTitle from "@/common/utils/page-title";
 import PageHeading from "@/components/pageHeading";
 import Work from "@/components/work";
 import Notice from "@/components/work/Notice";
 import { Metadata } from "next";
+import { useEffect } from "react";
 
 export const metadata: Metadata = pageTitle.work;
 
 const WorkPage = () => {
+   useEffect(() => {
+      if (typeof window !== 'undefined') {
+        // Code that relies on document object (only runs in the browser)
+        document.title = "My Dynamic Title";
+      }
+    }, []);
    return (
       <div>
          <PageHeading>Process</PageHeading>
