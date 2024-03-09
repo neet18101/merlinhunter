d086df4fefaf8173f406024a860049b0514f6736
Navneet <amitkawasthi1@gmail.com>
2024-03-09 12:39:12 +0530

    new changes

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-import { useEffect } from 'react'; // Import useEffect hook
 import pageTitle from "@/common/utils/page-title";
 import PageHeading from "@/components/pageHeading";
 import Work from "@/components/work";
@@ -9,12 +7,6 @@ import { Metadata } from "next";
 export const metadata: Metadata = pageTitle.work;
 
 const WorkPage = () => {
-   useEffect(() => {
-      // Code that accesses document
-      // This code will only be executed in the browser environment
-      // You can put your document-dependent logic here
-   }, []); // Empty dependency array ensures the effect runs only once, similar to componentDidMount
-
    return (
       <div>
          <PageHeading>Process</PageHeading>
